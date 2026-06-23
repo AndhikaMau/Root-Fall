4058e2d19150422d16a8b01939f046230800b13f
dikapol1229 <dikapol1229@gmail.com>
2026-06-23 12:06:16 +0700

    awd

--- Assets/Script/PlayerHealth.cs
+++ Assets/Script/PlayerHealth.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 3;
+    public float invincibleTime = 1f;
 
     private int currentHealth;
     private Animator anim;
     private Rigidbody2D rb;
+    private SpriteRenderer sr;
+
+    private bool isInvincible;
 
     public bool IsDead { get; private set; }
 
@@ -16,25 +21,52 @@ public class PlayerHealth : MonoBehaviour
 
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(int damage)
     {
-        if (IsDead)
+        if (IsDead || isInvincible)
             return;
 
         currentHealth -= damage;
 
         Debug.Log("Player HP: " + currentHealth);
 
+        StartCoroutine(DamageFlash());
+
         if (currentHealth > 0)
         {
             anim.SetTrigger("Hurt");
+            StartCoroutine(Invincibility());
         }
         else
         {
             Die();
         }
+    }
+
+    IEnumerator DamageFlash()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            sr.color = Color.red;
+
+            yield return new WaitForSeconds(0.08f);
+
+            sr.color = Color.white;
+
+            yield return new WaitForSeconds(0.08f);
+        }
+    }
+
+    IEnumerator Invincibility()
+    {
+        isInvincible = true;
+
+        yield return new WaitForSeconds(invincibleTime);
+
+        isInvincible = false;
     }
 
     void Die()
@@ -44,15 +76,12 @@ public class PlayerHealth : MonoBehaviour
 
         IsDead = true;
 
-        // Hentikan gerakan horizontal saja
         rb.linearVelocity =
             new Vector2(0f, rb.linearVelocity.y);
 
-        // Reset parameter animator
         anim.SetFloat("Speed", 0);
-        anim.SetBool("IsGrounded", true);
 
-        // Paksa masuk animasi mati
+        // Paksa langsung masuk animasi mati
         anim.Play("playerdeath", 0, 0f);
 
         Debug.Log("Player Mati");
